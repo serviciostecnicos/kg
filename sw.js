f0e2636b8b1eade5f6d7d545649b5fd4c7d957ec
serviciostecnicos <69409124+serviciostecnicos@users.noreply.github.com>
2020-08-21 17:04:15 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ const CACHE_NAME = 'v1_cache_programador_fitness',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './css/img/incos.png'
+    './css/img/icons.png'
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
